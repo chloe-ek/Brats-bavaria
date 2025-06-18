acebf0c633d8e305a22eec7170ecef175a391632
chloe-ek <chloe.kwon08@gmail.com>
2025-06-18 15:00:28 -0700

    Add link to go back homepage

--- src/app/submit/success/page.tsx
+++ src/app/submit/success/page.tsx
@@ -2,6 +2,7 @@
 
 import Footer from '@/components/Footer';
 import Nav from '@/components/Nav';
+import Link from 'next/link';
 
 export default function SubmitSuccessPage() {
   return (
@@ -12,6 +13,11 @@ export default function SubmitSuccessPage() {
           <h1 className="text-3xl sm:text-4xl font-bold mb-6">Application Submitted!</h1>
           <p className="mb-10">We&apos;ll review your application and contact you soon.</p>
         </div>
+        <Link 
+          href="/" 
+          className="h-10 bg-[#f6f6f6] px-4 text-sm font-bold text-black flex items-center justify-center">
+          Return Home
+        </Link>
       </main>
       <Footer />
     </div>
