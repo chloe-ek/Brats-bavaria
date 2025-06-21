0ab25a665e1260dfe064535e5e8b64dac71be8a2
chloe-ek <chloe.kwon08@gmail.com>
2025-06-20 17:41:08 -0700

    Add payment-success and cancel pages

---
+++ src/app/payment-success/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Footer from '@/components/Footer';
+import Nav from '@/components/Nav';
+import Link from 'next/link';
+
+export default function PaymentSuccessPage() {
+  return (
+    <div className="bg-[#111518] text-white min-h-screen flex flex-col">
+      <Nav />
+      <main className="flex-1 px-4 sm:px-6 py-10 w-full max-w-[1400px] mx-auto">
+        <div className="text-center">
+          <h1 className="text-3xl sm:text-4xl font-bold mb-6">Payment Successful!</h1>
+          <p className="mb-10">Thanks for your payment. You&apos;re all set for the event!</p>
+        </div>
+        <Link 
+          href="/" 
+          className="h-10 bg-[#f6f6f6] px-4 text-sm font-bold text-black flex items-center justify-center">
+          Return Home
+        </Link>
+      </main>
+      <Footer />
+    </div>
+  );
+}
