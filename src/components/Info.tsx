54eedae816a7de3e5f94b129d02b6cb1ff831f5c
chloe-ek <chloe.kwon08@gmail.com>
2025-07-03 08:53:39 -0700

    Fix the typo

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -14,7 +14,7 @@ const Info = () => {
             Event Info
           </h2>
           <p className="text-sm sm:text-base font-sans mb-8 sm:mb-10 leading-relaxed max-w-2xl">
-            Find us at Classic Garage in Richnmod. Check the cards below to learn more
+            Find us at Classic Garage in Richmond. Check the cards below to learn more
             about the event experience and schedule.
           </p>
 
