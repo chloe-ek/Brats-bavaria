7cd1fb41f8ea5d08f18e668bf7222ae9009296ff
chloe-ek <chloe.kwon08@gmail.com>
2026-04-22 12:31:36 -0700

    Update the next.js config file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
   images: {
-    domains: ['res.cloudinary.com'], 
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+  turbopack: {
+    root: __dirname,
   },
 };
 
